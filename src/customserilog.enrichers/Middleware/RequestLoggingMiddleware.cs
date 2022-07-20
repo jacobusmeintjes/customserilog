5810e20ad56e94e11d025fe1095ee1be7a927dc4
Jacobus Meintjes <jacobus.meintjes@gmail.com>
2022-07-20 22:23:05 +0200

    read request and response body content

--- src/customserilog.enrichers/Middleware/RequestLoggingMiddleware.cs
+++ src/customserilog.enrichers/Middleware/RequestLoggingMiddleware.cs
@@ -18,19 +18,42 @@ public class RequestLoggingMiddleware
 
     public async Task Invoke(HttpContext context)
     {
+        Stream originalBody = context.Response.Body;
+
+        string responseBody = string.Empty;
+        string requestBody = string.Empty;
         try
         {
-            await _next(context);
+            context.Request.EnableBuffering();
+            using (var streamReader = new StreamReader(
+                       context.Request.Body, System.Text.Encoding.UTF8, leaveOpen: true))
+                requestBody = await streamReader.ReadToEndAsync();
+
+            context.Request.Body.Position = 0;
+
+
+            using (var memStream = new MemoryStream())
+            {
+                context.Response.Body = memStream;
+                await _next(context);
+
+                memStream.Position = 0;
+                responseBody = await new StreamReader(memStream).ReadToEndAsync();
+
+                memStream.Position = 0;
+                await memStream.CopyToAsync(originalBody);
+            }
         }
         finally
         {
+            //var requestBody = new StreamReader(context.Request.Body).ReadToEnd();
+            //var responseBody = new StreamReader(context.Response.Body).ReadToEnd();
             _logger.LogInformation(
-                "Request {Method} {Url} => {StatusCode} {ContentType} {Response}",
+                "Request {Method} {Url} => {StatusCode} {ContentType} {RequestBody} {ResponseBody}",
                 context.Request?.Method,
                 context.Request?.Path.Value,
                 context.Response?.StatusCode,
-                context.Response.ContentType,
-                context.Response.Body);
+                context.Response.ContentType, requestBody, responseBody);
         }
     }
 }
